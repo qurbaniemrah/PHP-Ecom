2e06d609ab89e8415004b6355b9a33e9f156725f
qurbaniemrah <qurbaniemrah@outlook.com>
2023-01-31 17:56:49 +0400

    updated filter

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $("#banner-area .owl-carousel").owlCarousel({
-        dots:true,
-        items:1
+        dots: true,
+        items: 1
     });
 
     $("#top-sale .owl-carousel").owlCarousel({
-        loop:true,
-        nav:true,
-        dots:false,
-        responsive:{
-            0:{
-                items:1
+        loop: true,
+        nav: true,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
             },
 
             600: {
-                items:3
+                items: 3
             },
 
             1000: {
-                items:5
+                items: 5
             }
 
-    }
+        }
     });
+
+    var $grid = $(".grid").isotope({
+        itemSelector : '.grid-item',
+        layoutMode : 'fitRows'
+    });
+
+    // filter items on button click
+    $(".button-group").on("click", "button", function(){
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({ filter: filterValue});
+    })
 });
