db8d1460fa7c2d909ddc92936a07ddd27187f12b
qurbaniemrah <qurbaniemrah@outlook.com>
2023-02-03 19:47:31 +0400

    updated

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
-$(document).ready(function () {
+$(document).ready(function(){
+
+    // banner owl carousel
     $("#banner-area .owl-carousel").owlCarousel({
         dots: true,
         items: 1
     });
 
+    // top sale owl carousel
     $("#top-sale .owl-carousel").owlCarousel({
         loop: true,
         nav: true,
         dots: false,
-        responsive: {
+        responsive : {
             0: {
                 items: 1
             },
-
             600: {
                 items: 3
             },
-
-            1000: {
+            1000 : {
                 items: 5
             }
-
         }
     });
 
+    // isotope filter
     var $grid = $(".grid").isotope({
         itemSelector : '.grid-item',
         layoutMode : 'fitRows'
@@ -35,41 +36,65 @@ $(document).ready(function () {
         $grid.isotope({ filter: filterValue});
     })
 
-    // New Phones Owl Carousel
+
+    // new phones owl carousel
     $("#new-phones .owl-carousel").owlCarousel({
         loop: true,
         nav: false,
         dots: true,
-        responsive: {
+        responsive : {
             0: {
                 items: 1
             },
-
             600: {
                 items: 3
             },
-
-            1000: {
+            1000 : {
                 items: 5
             }
-
         }
     });
-// Blogs owl carousel
-$("#blogs .owl-carousel").owlCarousel({
-    loop: true,
-    nav: false,
-    dots: true,
-    responsive: {
-        0: {
-            items: 1
-        },
 
-        600: {
-            items: 3
-        },
-    }
-})
+    // blogs owl carousel
+    $("#blogs .owl-carousel").owlCarousel({
+        loop: true,
+        nav: false,
+        dots: true,
+        responsive : {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            }
+        }
+    })
+
+
+    // product qty section
+    let $qty_up = $(".qty .qty-up");
+    let $qty_down = $(".qty .qty-down");
+    // let $input = $(".qty .qty_input");
+
+    // click on qty up button
+    $qty_up.click(function(e){
+        let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
+        if($input.val() >= 1 && $input.val() <= 9){
+            $input.val(function(i, oldval){
+                return ++oldval;
+            });
+        }
+    });
+
+       // click on qty down button
+       $qty_down.click(function(e){
+        let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
+        if($input.val() > 1 && $input.val() <= 10){
+            $input.val(function(i, oldval){
+                return --oldval;
+            });
+        }
+    });
 
 
 });
