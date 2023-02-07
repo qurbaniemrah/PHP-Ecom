142b015b3adc301d6643f2296016d76f22c7effb
qurbaniemrah <qurbaniemrah@outlook.com>
2023-02-07 12:19:49 +0400

    shopping cart template added

---
+++ cart.php
@@ -0,0 +1,19 @@
+<?php
+// include header file
+include("header.php");
+?>
+
+<?php
+
+// include top-sale file
+include("Template/_cart-template.php");
+
+// include top-sale file
+include("Template/_new-phones.php");
+
+?>
+
+<?php
+// include footer.php file
+include("footer.php");
+?>
---
+++ product.php
@@ -0,0 +1,19 @@
+<?php
+// include header file
+include("header.php");
+?>
+
+<?php
+
+// include top-sale file
+include("Template/_products.php");
+
+// include top-sale file
+include("Template/_top-sale.php");
+
+?>
+
+<?php
+// include footer.php file
+include("footer.php");
+?>
